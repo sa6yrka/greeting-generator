cfed625eae612ff164f5ffe5e5389c7e7d5a235c
Sabyrka <sa6yrka@gmail.com>
2026-01-21 13:15:19 +0500

    feat: implement occasion selection

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,5 +1,15 @@
+import { useState } from 'react'
+import { OccasionType } from '../types/occasionType'
+
 export const Editor = () => {
+  const [occasion, setOccasion] = useState<OccasionType>(OccasionType.BIRTHDAY)
+
   return (
-    <div className='max-w-7xl mx-auto'></div>
+    <div className='max-w-7xl mx-auto'>
+      <p>{occasion}</p>
+
+      <button onClick={() => setOccasion(OccasionType.BIRTHDAY)}>День Рождения</button>
+      <button onClick={() => setOccasion(OccasionType.NEW_YEAR)}>Новый Год</button>
+    </div>
   )
 }
---
+++ src/types/occasionType.ts
@@ -0,0 +1,4 @@
+export enum OccasionType {
+  BIRTHDAY = 'День Рождения',
+  NEW_YEAR = 'Новый Год',
+}
